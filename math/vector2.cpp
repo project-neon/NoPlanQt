231ccaa2477d6803ab6e27d39e4e907a66cecd0d
Gabriel Mendes <mendes.gabriel95@gmail.com>
2018-06-16 16:38:14 -0300

    Add math operations to Vector2 class

--- math/vector2.cpp
+++ math/vector2.cpp
@@ -1,33 +1,53 @@
-// Vector2.cpp
 #include "math/vector2.h"
 
-Vector2::Vector2(void)
-{
-}
+Vector2::Vector2(void) {}
 
-Vector2::Vector2(float X, float Y){
+Vector2::Vector2(float X, float Y) {
+
     this->X = X;
     this->Y = Y;
+
 }
 
-// Returns the length of the vector
-float Vector2::Length(){
+float Vector2::Length() {
+
     return sqrt(X * X + Y * Y);
+
 }
 
 // Normalizes the vector
-Vector2 Vector2::Normalize(){
+Vector2 Vector2::Normalize() {
+
     Vector2 vector;
     float length = this->Length();
 
-    if(length != 0){
+    if(length != 0) {
         vector.X = X/length;
         vector.Y = Y/length;
     }
 
     return vector;
+
 }
 
-Vector2::~Vector2(void)
-{
+Vector2::~Vector2(void) {}
+
+static float sum(Vector2 v1, Vector2 v2) {
+    return Vector2 (v1.x + v2.x, v1.y + v2.y);
+}
+static float sub(Vector2 v1, Vector2 v2) {
+    return Vector2 (v1.x - v2.x, v1.y - v2.y);
+}
+static float mult(Vector2 v1, Vector2 v2) {
+    return Vector2 (v1.x * v2.x, v1.y * v2.y);
+}
+static float div(Vector2 v1, Vector2 v2) {
+    return Vector2 (v1.x / v2.x, v1.y / v2.y);
+}
+//Theta has to be in radians
+static float fromTheta(float theta) {
+    return Vector2 (qCos(theta), qSin(theta));
+}
+static float angle(Vector2 v1) {
+    return qAtan2(v1.x, v1.y);
 }
--- math/vector2.h
+++ math/vector2.h
@@ -1,14 +1,27 @@
 #pragma once
- 
+
 #include <math.h>
- 
-class Vector2
-{
+#include <QtMath>
+
+class Vector2 {
 public:
+
+    float X,Y;
+
     Vector2(void);
     Vector2(float X, float Y);
     ~Vector2(void);
+
     float Length();
     Vector2 Normalize();
-    float X,Y;
+
+    static float sum(Vector2 v1, Vector2 v2);
+    static float sub(Vector2 v1, Vector2 v2);
+    static float mult(Vector2 v1, Vector2 v2);
+    static float div(Vector2 v1, Vector2 v2);
+    static float fromTheta(float theta);
+    static float angle(Vector2 v1, Vector2 v2);
+    static float angleBetween(Vector2 v1, Vector2 v2);
+    static float angleBetween(Vector2 v1, float theta);
+
 };
